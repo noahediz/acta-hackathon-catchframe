75d1399a8c1ca505247a32783e830db8bc17726f
Noah Ediz <noah.ediz@outlook.com>
2025-10-04 15:11:42 +0200

    Download raw video in processing service

--- services/processing/main.go
+++ services/processing/main.go
@@ -3,10 +3,13 @@ package processing
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"os"
 
 	"cloud.google.com/go/firestore"
+	"cloud.google.com/go/storage"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 )
@@ -22,10 +25,12 @@ type PubSubMessage struct {
 }
 
 var (
-	firestoreClient *firestore.Client
-
-	gcpProjectID      = os.Getenv("GOOGLE_CLOUD_PROJECT")
-	reportsCollection = "reports"
+	firestoreClient        *firestore.Client
+	storageClient          *storage.Client
+	gcpProjectID           = os.Getenv("GOOGLE_CLOUD_PROJECT")
+	reportsCollection      = "reports"
+	rawUploadsBucketName   = "catchframe-raw-uploads"       // NEW: Name of the input bucket
+	processedReportsBucket = "catchframe-processed-reports" // NEW: Name of the output bucket
 )
 
 func init() {
@@ -52,15 +57,41 @@ func ProcessingService(ctx context.Context, e cloudevents.Event) error {
 	reportID := string(msg.Message.Data)
 	log.Printf("Received request to process report: %s", reportID)
 
-	// For now, just update the status in Firestore to test the code
-	_, err := firestoreClient.Collection(reportsCollection).Doc(reportID).Update(ctx, []firestore.Update{
-		{Path: "status", Value: "completed"},
+	// Define temporary file paths.
+	rawVideoFileName := reportID + ".webm"
+	localRawPath := "/tmp/" + rawVideoFileName
+
+	// Delete temporary raw files
+	defer os.Remove(localRawPath)
+
+	// Download raw video from GCS.
+	rawObject := storageClient.Bucket(rawUploadsBucketName).Object(rawVideoFileName)
+	rc, err := rawObject.NewReader(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to create GCS reader: %w", err)
+	}
+	defer rc.Close()
+
+	localFile, err := os.Create(localRawPath)
+	if err != nil {
+		return fmt.Errorf("failed to create local file: %w", err)
+	}
+	defer localFile.Close()
+
+	if _, err := io.Copy(localFile, rc); err != nil {
+		return fmt.Errorf("failed to copy GCS object to local file: %w", err)
+	}
+	log.Printf("Successfully downloaded raw video to %s", localRawPath)
+
+	// Update the status in Firestore
+	_, err = firestoreClient.Collection(reportsCollection).Doc(reportID).Update(ctx, []firestore.Update{
+		{Path: "status", Value: "downloaded"},
 	})
 	if err != nil {
 		log.Printf("Error updating firestore document %s: %v", reportID, err)
 		return err
 	}
 
-	log.Printf("Successfully processed report: %s. Status updated to 'completed'.", reportID)
+	log.Printf("Report %s status updated to 'downloaded'.", reportID)
 	return nil
 }
